614efd930ac3aabb48023f3b36bfa758e4638ac7
gabrielbriks <37519878+gabrielbriks@users.noreply.github.com>
2019-12-03 09:56:17 -0300

    Add script Environment Install Script

---
+++ EnvironmentInstallScript.ps1
@@ -0,0 +1,43 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+Write-Host "MODO ADM ";
+Write-Host " ";
+Write-Host " ";
+Write-Host "... INICIANDO CONFIGURACAO ..." -foregroundcolor "magenta";
+
+
+
+$chocoAtivo = choco -v;
+
+if($chocoAtivo){
+	Write-Host " ";
+	Write-Host "Chocolatey Ativo";
+	Write-Host " ";
+}
+else{
+	Write-Host " ";
+	Write-Host "Chocolatey Inativo";
+	Write-Host " ";
+	Write-Host "INICIANDO INSTALACAO CHOCOLATEY" -foregroundcolor "magenta";
+
+	Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+	Write-Host " ";
+
+}
+
+cinst -y vlc #visualstudio2019enterprise #sql-server-management-studio #git #googlechrome #firefox #notepadplusplus.install #lightshot #server-jre10 #python3 
+
+Write-Host "PACOTES INSTALADOS: "
+Write-Host " ";
+choco list -l
+
+
+
+Write-Host " ";
+Write-Host " ";
+Write-Host " ";
+Write-Host "           " " By GitHub - GabrielBriks" "                         Grato em ajudar...";
+
+
+Read-host
